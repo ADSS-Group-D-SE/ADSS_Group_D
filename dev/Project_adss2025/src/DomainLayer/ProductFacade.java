c9b35c2ca6542039080c5c30338a49f85c9a9cf0
Ori Attar <oriatta@post.bgu.ac.il>
2026-04-11 18:07:02 +0300

    Faulty product functionality, and purchase method

--- dev/Project_adss2025/src/DomainLayer/ProductFacade.java
+++ dev/Project_adss2025/src/DomainLayer/ProductFacade.java
@@ -1,4 +1,100 @@
 package DomainLayer;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 public class ProductFacade {
+    private HashMap<String, ProductDL> products;
+    private HashMap<Integer,FaultyProductDL> faultyProducts;
+
+    private int faultyProductsIdCounter=0;
+
+    public ProductFacade(){
+        products=new HashMap<String, ProductDL>();
+        faultyProducts=new HashMap<Integer, FaultyProductDL>();
+    }
+
+    private int generateNextId() {
+        return faultyProductsIdCounter++;
+    }
+
+    /**
+    A method that locates a product by its catalog number and returns it.
+    throws exception if product was not located.
+     **/
+    public ProductDL FindProductByID(String catalog_number)
+    {
+        ProductDL p = this.products.get(catalog_number);
+        if(p == null)
+            throw new NoSuchElementException("Product:" +catalog_number +" ,No such product was found in facade");
+        return p;
+    }
+
+    /**
+    A method that locates a faulty report entry in map, by its report id.
+    if wasnt found, throws an exception.
+     **/
+    public FaultyProductDL FindProductByReportId(int report_id)
+    {
+        FaultyProductDL p = this.faultyProducts.get(report_id);
+        if(p == null)
+            throw new NoSuchElementException("Report:"+ report_id +" ,No such report was found in facade");
+        return p;
+    }
+
+    /**
+    Method that creates a faulty product record on the faulty product map.
+    Looks for the product by catalog, throws exception if wasnt found.
+    returns report id to the client
+     **/
+    public Integer ReportFaultyProduct(String catalog_number, String description, LocalDateTime dateOnReport)
+    {
+        ProductDL toFaulty = FindProductByID(catalog_number);
+        FaultyProductDL toAdd = new FaultyProductDL(toFaulty,generateNextId(),description,dateOnReport);
+
+        this.faultyProducts.put(toAdd.getReportID(), toAdd);
+        return toAdd.getReportID();
+    }
+
+    /**
+    Method that removes a report from the map, locates it by its ID.
+    If the report wasnt found, throws.
+     **/
+    public void RemoveFaultyReport(int report_id)
+    {
+        FaultyProductDL p = FindProductByReportId(report_id);
+        this.faultyProducts.remove(p.getReportID());
+    }
+
+    /**
+    Method that iterates on the faulty product map, and adds each entry that fit the entered date range.
+    construct a string reports and returns it.
+     **/
+    public String CreateFaultyReport(LocalDateTime start,LocalDateTime end)
+    {
+        String report = "Fault product reports from " + start.toString() +" to " + end.toString() +"\n===========================================\n";
+        for(Map.Entry<Integer,FaultyProductDL> en: this.faultyProducts.entrySet())
+        {
+            FaultyProductDL p = en.getValue();
+            if(!p.getDateOnReport().isAfter(end) && !p.getDateOnReport().isBefore(start)) // if the report date fits: start <+ report date <= end
+            {
+                report += "Report id:" + p.getReportID() +"\nOn product:" +p.getName() + ", Catalog number:" + p.getCatalog_number() + " ,Location:" +p.getLocation() +"\n" +
+                        "Reported on:" + p.getDateOnReport().toString() +"\nDescription:\n" + p.getDescription() +"\n\n";
+            }
+        }
+        return report;
+    }
+
+    /**
+     Method that finds a product by its catalog number, and conducts a purchase call to it.
+     ALLOWS negative number, for amount increase.
+     **/
+    public void PurchaseProduct(String catalog_number,int shelves,int stock)
+    {
+        ProductDL p = FindProductByID(catalog_number);
+        p.Purchase(shelves,stock);
+    }
+
 }
--- dev/Project_adss2025/src/ServiceLayer/ProductServices.java
+++ dev/Project_adss2025/src/ServiceLayer/ProductServices.java
@@ -1,4 +1,110 @@
 package ServiceLayer;
 
+import DomainLayer.ProductFacade;
+
+import java.time.LocalDateTime;
+
+
 public class ProductServices {
+    private static ProductServices INSTANCE;
+    private final ProductFacade pFacade;
+
+    /**
+     *Initializes the connection to the Domain Layer with the ProductFacade.
+     */
+    private ProductServices(){
+        this.pFacade=new ProductFacade();
+    }
+
+
+    /**
+     * Provides a global point of access to the ProductServices instance.
+     * @return the single instance of ProductServices
+     */
+    public static ProductServices getInstance(){
+        if(INSTANCE==null){
+            INSTANCE= new ProductServices();
+        }
+        return INSTANCE;
+    }
+
+    /**
+    Service method for the product reporting service.
+    Returns response: with report ID if operation was successful
+                      else, return response with error msg.
+     **/
+    public Response<Integer> ReportFaultyProduct(String catalog_number, String description, LocalDateTime dateOnReport)
+    {
+        Response<Integer> res = null;
+        try
+        {
+            res = new Response<>(null,this.pFacade.ReportFaultyProduct(catalog_number,description,dateOnReport));
+        }
+        catch (Exception e)
+        {
+            res = new Response<>(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+    Service that allows removing a faulty product report by its id.
+    Returns: Response with null value if operation was successful.
+             else: returns a response with error msg.
+     **/
+    public Response<String> RemoveFaultyReport(int report_id)
+    {
+        Response<String> res = null;
+        try
+        {
+            this.pFacade.RemoveFaultyReport(report_id);
+            res = new Response<>(null,null);
+        }
+        catch (Exception e)
+        {
+            res = new Response<>(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+    Service for the faulty products report service.
+    returns response: with the product report if operation was successful.
+                      else, returns error msg.
+
+     **/
+    public Response<String> CreateFaultyProductReport(LocalDateTime start,LocalDateTime end)
+    {
+        Response<String> res = null;
+        try
+        {
+            res = new Response<>(null,this.pFacade.CreateFaultyReport(start,end));
+        }
+        catch (Exception e)
+        {
+            res = new Response<>(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+    Service for the purchase method ,mainly for testing purposes.
+    Returns response: with null value if operation was successful.
+    else : with error msg.
+     **/
+    public Response<String> PurchaseProduct(String catalog_number,int shelves,int stock)
+    {
+        Response<String> res = null;
+        try
+        {
+            this.pFacade.PurchaseProduct(catalog_number,shelves,stock);
+            res = new Response<>(null,null);
+        }
+        catch (Exception e)
+        {
+            res = new Response<>(e.getMessage());
+        }
+        return res;
+    }
+
 }
